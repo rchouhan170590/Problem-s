0b4e5e945b9962e438d62262919673290232e648
ROHIT CHOUHAN <43743224+rchouhan170590@users.noreply.github.com>
2019-04-07 11:37:03 +0530

    Update ActivitySelectionProblem.c

--- ActivitySelectionProblem.c
+++ ActivitySelectionProblem.c
@@ -2,7 +2,7 @@
 // https://github.com/rchouhan170590/GreedyAlgorithms/blob/master/ActivitySelectionProblem.c
 
 #include<stdio.h>
-int MaxActivity(int S[],int F[],int ,int,int);
+int MaxActivity(int S[],int F[],int ,int,int,int hold[]);
 int main()
 {
    int size;
@@ -13,22 +13,27 @@ int main()
      scanf("%d",&S[k]);
    for(int k=0;k<size;k++)
       scanf("%d",&F[k]);
-
-   int a=MaxActivity(S,F,size,-1,0);
-   printf("%d",a);
+   int hold[size];    //hold the index of selected activity.
+   int a=MaxActivity(S,F,size,-1,0,hold);
+   printf("Total Number of activtiy : %d\n",a);
+   printf("index of selected activity : ");
+   for(int i=0;i<a;i++)
+      printf("%d ",hold[i]);
    return 0;
 }
 
-int MaxActivity(int S[],int F[],int size,int end,int i)
+int MaxActivity(int S[],int F[],int size,int end,int i,int hold[])
 {
    if(i>=size)
      return 0;
    int b=0;
-   int a=MaxActivity(S,F,size,end,i+1);
+   int a=MaxActivity(S,F,size,end,i+1,hold);
    if(end<=S[i])
    {
-     b =1+MaxActivity(S,F,size,F[i],i+1);
+     b =1+MaxActivity(S,F,size,F[i],i+1,hold);
    }
    int c=(a>b?a:b);
+   if(b>a)
+      hold[c-1]=i;
    return c;
 }
